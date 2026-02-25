668eb7733ed091c02ca454f59be1a99186d49cdd
SANJAYKANNAN002 <kannansanjay0912@gmail.com>
2026-02-25 10:07:42 +0530

    this is UC5

--- PalindromeApp/src/PalindromeCheckerApp.java
+++ PalindromeApp/src/PalindromeCheckerApp.java
@@ -1,6 +1,35 @@
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
+        // Hardcoded string
+        String word = "madam";
+
+        // Create Stack of Characters
+        Stack<Character> stack = new Stack<>();
+
+        // Push each character into stack
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        // Pop characters and compare
+        for (int i = 0; i < word.length(); i++) {
+            char poppedChar = stack.pop();
+
+            if (word.charAt(i) != poppedChar) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println("The word \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The word \"" + word + "\" is NOT a Palindrome.");
+        }
 
 
 
